01b91a7dc2c8df8b3d27db56cefefc8a3e4fa2a3
soren@thomson <scott.soren@gmail.com>
2022-05-06 08:28:21 +0100

    clean up

--- demos/22E06_ICL/01_coplot_CV_cycles.py
+++ demos/22E06_ICL/01_coplot_CV_cycles.py
@@ -8,9 +8,7 @@ ec = Measurement.read(
 ec.plot()
 
 ec.calibrate(RE_vs_RHE=0.720, A_el=0.196)
-
 cv = ec.as_cv()
-
 cv.plot()
 
 cv.plot_cycles()
@@ -18,7 +16,6 @@ cv.plot_cycles()
 ax = cv[1].plot(color="k", label="early")
 cv[200].plot(color="b", ax=ax, label="late")
 ax.legend()
-
 
 cv.redefine_cycle(start_potential=0.5, redox=True)
 
