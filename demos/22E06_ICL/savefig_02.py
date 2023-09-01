75f95277cb67a875bb9e7b646087f2142d63752c
soren@thomson <scott.soren@gmail.com>
2023-09-01 15:36:15 +0200

    updates for ISE poster

--- demos/22E06_ICL/savefig_02.py
+++ demos/22E06_ICL/savefig_02.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from ixdat import Measurement
+from matplotlib import pyplot as plt
 
-data_dir = Path("../../data/02")
+
+data_dir = Path("../data/02")
 
 sec_meas = Measurement.read(
     data_dir / "test-7SEC.csv",
@@ -17,4 +19,33 @@ sec_meas.set_reference_spectrum(V_ref=0.6)
 
 ax = sec_meas.plot_waterfall()
 
-ax.get_figure().savefig("demo_02.png")
+r =  0.85
+fig = ax.get_figure()
+fig.set_figwidth(6 * r)
+fig.set_figheight(4.5 * r)
+fig.tight_layout()
+fig.savefig("demo_02.png", dpi=600)
+
+axes2 = sec_meas.plot_measurement()
+axes2[0].set_xlabel("time / [s]")
+fig = axes2[0].get_figure()
+fig.set_figwidth(6 * r)
+fig.set_figheight(4.5 * r)
+fig.tight_layout()
+fig.savefig("demo_02_measurement.png", dpi=600)
+
+r = 0.75
+ax3 = sec_meas.get_dOD_spectrum(t=300).plot()
+fig = ax3.get_figure()
+fig.set_figwidth(6 * r)
+fig.set_figheight(4.5 * r)
+fig.tight_layout()
+fig.savefig("demo_02_spectrum.png", dpi=600)
+
+r = 0.85
+sec_meas.plot_wavelengths_vs_potential(wavelengths=["w800", "w580", "w480"])
+fig = plt.gcf()  # FIXME: plot_wavelengths_vs_potential doesn't return an axis!
+fig.set_figwidth(6 * r)
+fig.set_figheight(4.5 * r)
+fig.tight_layout()
+fig.savefig("demo_02_wavelengths.png", dpi=600)
